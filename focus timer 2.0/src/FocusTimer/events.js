ff8911bb404d6bf1a9997e21e84b049a252be645
Luiz Felipe de Vila <116048208+LuizdeVila@users.noreply.github.com>
2024-03-16 13:34:11 -0300

    Add files via upload

---
+++ focus timer 2.0/src/FocusTimer/events.js
@@ -0,0 +1,26 @@
+import * as elements from './elements.js';
+import * as actions from './actions.js'
+
+export function registerControls() {
+  elements.controls.addEventListener('click', (event) => {
+    const action = event.target.dataset.action
+
+    if(typeof actions[action]() != "function") {
+      return
+    }
+
+    actions[action]()
+  })
+}
+
+export function registerSounds() {
+  elements.sounds.addEventListener('click', (event) => {
+    const sound = event.target.dataset.sound
+
+    if (typeof actions[sound]() != "function") {
+      return
+    }
+
+    actions[sound]()
+  })
+}
